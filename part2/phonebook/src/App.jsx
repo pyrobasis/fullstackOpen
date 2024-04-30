1fc5056d7751a0e9b41752ad66e69e7a3b9101ce
Pyrobasis <pyrobasis.seo@gmail.com>
2024-05-01 00:15:03 +0900

    phonebook step 5

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,34 @@
 import { useState } from 'react'
 
-const Entries = ({props}) => {
-  console.log(props);
+const Filter = ({setFilterWord}) => {
+  const [newSearch, setNewSearch] = useState('')
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value)
+    setFilterWord(event.target.value)
+  }
+  return (
+    <div>
+    filter shown with <input value={newSearch} onChange={handleSearchChange} />
+    </div>
+  )
+}
+
+const Entries = ({persons, filter}) => {
+  
+  const filteredPersons = persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
+  
   return (
     <ul>
-      {props.map(p => {
+      {filteredPersons.map(p => {
         return <li key={p.id}>{p.name} {p.number}</li>
       })}
     </ul>
   )
 }
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+const InputPerson = ({props, handler}) => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [newSearch, setNewSearch] = useState('')
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
@@ -30,47 +38,53 @@ const App = () => {
     setNewName(event.target.value)
   }
 
-  const handleSearchChange = (event) => {
-    setNewSearch(event.target.value)
-  }
-
   const submitPerson = (event) => {
     event.preventDefault()    
 
-    const checker = persons.some(p => p.name === newName)
+    const checker = props.some(p => p.name === newName)
     
     if(checker){
       alert(`${newName} is already added to phonebook`)
     } else {
-      console.log(checker, 'if false')
-      const newPerson = { name : newName, number : newNumber }  
-      setPersons(persons.concat(newPerson))
+      const newPerson = { name : newName, number : newNumber, id : props.length + 1 }  
+      handler(props.concat(newPerson))
     }
 
     setNewName('')
+    setNewNumber('')
   }
 
+  return (
+  <form onSubmit={submitPerson}>
+    <div>
+      name: <input value={newName} onChange={handleNoteChange}/>
+    </div>
+    <div>
+      number: <input value={newNumber} onChange={handleNumberChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>)
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [filterWord, setFilterWord] = useState('')
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={newSearch} onChange={handleSearchChange} />
-      </div>
+      <Filter setFilterWord={setFilterWord}/>
       <h2>add a new person</h2>
-      <form onSubmit={submitPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNoteChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <InputPerson props={persons} handler={setPersons} />
       <h2>Numbers</h2>
-      <Entries props={persons.filter(p => p.name.toLowerCase().includes(newSearch.toLowerCase()))} />
+      <Entries persons={persons} filter={filterWord} />
     </div>
   )
 }
