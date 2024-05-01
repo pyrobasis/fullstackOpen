bb4c2f7b80adab1f7ff291824cb9497cac2b140a
Pyrobasis <pyrobasis.seo@gmail.com>
2024-05-02 00:18:46 +0900

    phonebook step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({setFilterWord}) => {
   const [newSearch, setNewSearch] = useState('')
@@ -69,13 +70,19 @@ const InputPerson = ({props, handler}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [filterWord, setFilterWord] = useState('')
+
+  useEffect(() => {
+    console.log('effect');
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise resolved')
+      console.log(response.data)
+      setPersons(response.data)
+    })
+  }, [])
 
   return (
     <div>
