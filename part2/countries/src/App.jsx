c9adc6086cca00d0110adb7e1939fd76d999316b
Pyrobasis <pyrobasis.seo@gmail.com>
2024-05-09 14:23:28 +0900

    countries step 2

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,21 +1,14 @@
 import { useState, useEffect } from 'react'
 import countryServices from './services/country.js'
 
-const SearchResult = ({countries, countryName}) => {
-  console.log(countries);
-  if(countries) {
-    const filteredCountries = countries.filter(c => c.name.common.toLowerCase().includes(countryName.toLowerCase()))
-    
-    if(filteredCountries.length === 1){
-        const country = filteredCountries[0]
-        const countryLanguages = []
-        Object.keys(country.languages).forEach((l, i) => {
-          console.log(country.languages[l])
-          countryLanguages.push(<li key={l}>{country.languages[l]}</li>)
-        })
-      
-      return (
-        <div>
+const CountryDetail = ({country}) => {
+  const countryLanguages = []
+  Object.keys(country.languages).forEach((l, i) => {
+    countryLanguages.push(<li key={l}>{country.languages[l]}</li>)
+  })
+
+  return (<>
+          <div>
           <h1>{country.name.common}</h1>
           <p>Capital: {country.capital[0]}</p>
           <p>Area: {country.area}</p>
@@ -25,12 +18,43 @@ const SearchResult = ({countries, countryName}) => {
           </ul>
           <img src={country.flags.png} />
         </div>
+  </>)
+}
+
+const SearchResult = ({countries, countryName, setAllCountryList}) => {
+  const [countryNum, setCountryNum] = useState(null)
+
+  if(countries) {
+    const filteredCountries = countries.filter(c => c.name.common.toLowerCase().includes(countryName.toLowerCase()))
+   
+    // when viewing a specific country's details
+    if(countryNum != null){
+      const viewedCountry = countries.filter(c => c.cca2.includes(countryNum))
+      return (
+        <div>
+          <div>{viewedCountry[0].name.common}<button onClick={() => setCountryNum(null)}> hide</button></div>
+          <CountryDetail country={viewedCountry[0]} />
+        </div>
+      )
+    }
+
+ 
+    // if there's only one country show details of it
+    if(filteredCountries.length === 1){
+        const country = filteredCountries[0]      
+      return (
+        <CountryDetail country={country}/>
       )    
     }
     
-    return (   
+    // by default list all the filtered countries
+    return (
       <div>
-        {filteredCountries.map((c) => <p key={c.name.common}>{c.name.common}</p>)}
+        {filteredCountries.map((c, i) => {
+            return (
+              <div key={c.name.common}>{c.name.common} <button onClick={() => setCountryNum(c.cca2)}>show</button></div>)
+          })
+        }
       </div>
     )
   }
@@ -41,7 +65,10 @@ function App() {
   const [allCountryList, setAllCountryList] = useState(null)
   const typeCountryInput = (e) => setCountryName(e.target.value)
   
-  useEffect(() => {countryServices.getAll().then(res => setAllCountryList(res))},[])
+  useEffect(() => {countryServices.getAll().then(res => {
+    res.map(r => r.detailToggle = false)
+    setAllCountryList(res)
+  })},[])
 
   if(allCountryList)
   return (
@@ -49,7 +76,7 @@ function App() {
     <div>
       find countries <input value={countryName} onChange={typeCountryInput}></input>
     </div>
-    <SearchResult countries={allCountryList} countryName={countryName} />
+    <SearchResult countries={allCountryList} countryName={countryName} setAllCountryList={setAllCountryList} />
     </>
   )
 }
